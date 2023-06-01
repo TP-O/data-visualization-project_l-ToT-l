56d879536cb9ded931890928c15bb48f3e4e1817
TP-O <letranphong2k1@gmail.com>
2023-06-01 19:52:45 +0700

    Fix duplicate pie-chart

--- chart-1.js
+++ chart-1.js
@@ -1,6 +1,62 @@
+const select = document.querySelector('#select-date');
+
+d3.csv("https://raw.githubusercontent.com/TP-O/data-visualization-project_l-ToT-l/main/data.csv").then(function (data) {
+	data.map((d) => d['Date']).forEach(option => {
+		const optionElement = document.createElement('option');
+		optionElement.textContent = option;
+		optionElement.value = option;
+		select.appendChild(optionElement);
+	});
+
+	chart1(data)
+	select.addEventListener('input', () => {
+		chart1(data)
+	})
+})
+
+function chart1(data) {
+	const labelChart1 = ['Others']
+	const dataChart1 = [0]
+	let row = null
+
+	for (const d of data) {
+		if (d['Date'] === select.value) {
+			row = d
+		}
+	}
+
+	if (!row) {
+		return
+	}
+
+	Object.entries(row).forEach(([label, val]) => {
+		val = parseFloat(val, 10)
+		if (isNaN(val)) {
+			return
+		} else if (val <= 5) {
+			dataChart1[0] += val
+		} else {
+			labelChart1.push(label)
+			dataChart1.push(val)
+		}
+	})
+
+	drawChart1(labelChart1, dataChart1);
+}
+
 function drawChart1(label, data) {
+	var svg = document.querySelector("#chart-1 > svg")
+	if (svg) {
+		document.getElementById("chart-1").removeChild(svg);
+	}
+
+	svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+	svg.setAttribute("width", 400);
+	svg.setAttribute("height", 300);
+	document.getElementById("chart-1").appendChild(svg);
+
 	var colorFirstRow = data.map(() => randomRgbColor());
-	var svg = d3.select("#chart-1 > svg"),
+	var svg = d3.select("#chart-1 svg"),
 		width = svg.attr("width"),
 		height = svg.attr("height"),
 		radius = Math.min(width, height) / 2.8,
@@ -34,15 +90,15 @@ function drawChart1(label, data) {
 		.attr("d", arc);
 
 	arcs.append("text")
-  .attr("transform", d => {
-    const pos = arc.centroid(d);
-    const midAngle = Math.atan2(pos[1], pos[0]);
-    const x = Math.cos(midAngle) * (radius * 1.2);
-    const y = Math.sin(midAngle) * (radius * 1.2);
-    return `translate(${x},${y})`;
-  })
-  .attr("text-anchor", "middle")
-  .text(d => `${d.data.toFixed(2)}%`);
+		.attr("transform", d => {
+			const pos = arc.centroid(d);
+			const midAngle = Math.atan2(pos[1], pos[0]);
+			const x = Math.cos(midAngle) * (radius * 1.2);
+			const y = Math.sin(midAngle) * (radius * 1.2);
+			return `translate(${x},${y})`;
+		})
+		.attr("text-anchor", "middle")
+		.text(d => `${d.data.toFixed(2)}%`);
 
 	// Query to colorList
 	var colorList = document.querySelector("#color-list-1");
